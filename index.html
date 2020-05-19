4cf5e6ea18ecfb3f808cb71a4d02d4a4db39debd
iamsaloni <saloniguptasg23@gmail.com>
2020-05-19 23:48:11 +0530

    Initial Commit

---
+++ index.html
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Git Login App</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="main.js"></script>
+</head>
+<body>
+   <div>
+       <button id="login">
+           Upload Files to Drive
+       </button>
+   </div> 
+</body>
+</html>
---
+++ main.js
@@ -0,0 +1,49 @@
+$(document).ready(function(){
+     
+    // client id of the project
+
+    var clientId = "1079639197960-5gri25ju46t8l9jn36sobcj93jtbv8eg.apps.googleusercontent.com";
+
+    // redirect_uri of the project
+
+    var redirect_uri = "http://localhost/googleDrive/upload.html";
+
+    // scope of the project
+
+    var scope = "https://www.googleapis.com/auth/drive";
+
+    // the url to which the user is redirected to
+
+    var url = "";
+
+
+    // this is event click listener for the button
+
+    $("#login").click(function(){
+
+       // this is the method which will be invoked it takes four parameters
+
+       signIn(clientId,redirect_uri,scope,url);
+
+    });
+
+    function signIn(clientId,redirect_uri,scope,url){
+     
+       // the actual url to which the user is redirected to 
+
+       url = "https://accounts.google.com/o/oauth2/v2/auth?redirect_uri="+redirect_uri
+       +"&prompt=consent&response_type=code&client_id="+clientId+"&scope="+scope
+       +"&access_type=offline";
+
+       // this line makes the user redirected to the url
+
+       window.location = url;
+
+
+
+
+    }
+
+
+
+});
